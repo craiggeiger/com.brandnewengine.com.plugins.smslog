7bb2207dec257e0c8e0d1ed70e648a8cce9c8c2c
Craig Geiger <craig@brandnewengine.com>
2014-08-05 15:26:46 -0400

    initial commit

---
+++ www/smslog.js
@@ -0,0 +1,26 @@
+
+
+var SmslogLoader = function (require, exports, module) {
+    var exec = require("cordova/exec");
+    
+    function Smslog () {}
+        
+    Smslog.prototype.getSMSLog = function (successFunc, failFunc) {
+        exec(successFunc, failFunc, "Smslog","getSMSLog",[]);
+    };
+
+    Smslog.prototype.getTexts = function (params, phoneNumber, successFunc, failFunc) {
+        exec(successFunc, failFunc, "Smslog","getTexts",[]);
+    };
+
+    Smslog.prototype.getTextsAfter = function (params, successFunc, failFunc) {
+        exec(successFunc, failFunc, "Smslog","getTextsAfter",[]);
+    };
+    
+    var smsLog = new Smslog();
+    module.exports = smsLog;
+};
+
+SmslogLoader(require, exports, module);
+
+cordova.define("cordova/plugin/Smslog", SmslogLoader);
