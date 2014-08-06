f19a5f76871b358763cbc5455cf39bc2cd0dca85
Craig Geiger <craig@brandnewengine.com>
2014-08-06 09:09:32 -0400

    query updates

--- src/android/Smslog.java
+++ src/android/Smslog.java
@@ -193,8 +193,8 @@ public class Smslog extends CordovaPlugin {
 
 
 
-        // ORIG Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, "address=? AND date>=?", queryData, sortOrder);
-        Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, "date>=?", queryData, sortOrder);
+        //Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
 
 
 
